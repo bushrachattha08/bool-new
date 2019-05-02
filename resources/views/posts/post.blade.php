4f4f287118f28238d218c1ba7cab3dcdd4882569
Ace <ace.nasir86@gmail.com>
2019-05-01 21:04:09 -0600

    add post edit

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -8,7 +8,95 @@
                 <div class="card-header">Post</div>
 
                 <div class="card-body">
-            Post
+                <div class="row">
+                  <form method="POST" action="{{ url('addPost') }}"
+                  enctype="multipart/form-data">
+                      @csrf
+
+                      <div class="form-group row">
+                          <label for="post_title" class="col-md-4 col-form-label text-md-right">
+                          Title</label>
+                          <div class="col-md-6">
+                              <input id="post_title" type="text"
+                              class="form-control @error('post_title') is-invalid @enderror"
+                               name="post_title" value="{{ old('post_title') }}"
+                               required autocomplete="post_title" autofocus>
+
+                              @error('post_title')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+                      <div class="form-group row">
+                          <label for="post_body" class="col-md-4 col-form-label text-md-right">
+                            Post Body</label>
+                          <div class="col-md-6">
+                              <textarea id="post_body"
+                              class="form-control @error('post_body')
+                              is-invalid @enderror" name="post_body"
+                              value="{{ old('post_body') }}" required
+                            ></textarea>
+
+                              @error('post_body')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+                      <div class="form-group row">
+                          <label for="category_id" class="col-md-4 col-form-label text-md-right">
+                            Categories</label>
+
+                          <div class="col-md-6">
+                              <select id="category_id" type="category_id"
+                              class="form-control @error('category_id') is-invalid @enderror"
+                               name="category_id" required>
+                               <option value="">Select</option>
+                                @if(count($categories) > 0)
+                                @foreach($categories ->all() as $category)
+                                <option value="{{ $category->id }}">
+                                  {{ $category->category}}</option>
+                                @endforeach
+                                @endif
+                              </select>
+                              @error('category_id')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+                      <div class="form-group row">
+                          <label for="post_image" class="col-md-4 col-form-label text-md-right">
+                          Featured Image</label>
+                          <div class="col-md-6">
+                              <input id="post_image" type="file"
+                              class="form-control @error('designation') is-invalid @enderror"
+                              name="post_image" required>
+                              @error('post_image')
+                                  <span class="invalid-feedback" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                          </div>
+                      </div>
+
+
+                      <div class="form-group row mb-0">
+                          <div class="col-md-6 offset-md-4">
+                              <button type="submit" class="btn btn-primary btn-large btn-block">
+                                Publish Post
+                              </button>
+                          </div>
+                      </div>
+                  </form>
+                </div>
                 </div>
             </div>
         </div>
